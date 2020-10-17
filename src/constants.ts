ca729933b7fac9fd1b4d05fe3b9d959d3c53a247
aidigidreamers <61518519+aidigidreamers@users.noreply.github.com>
2020-10-17 12:32:37 +0500

    HASH Changed

--- src/constants.ts
+++ src/constants.ts
@@ -24,7 +24,9 @@ export enum Rounding {
 
 export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
 
-export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+export const INIT_CODE_HASH = '0x797caed26041f372a9954d9462cb5b084f6f78c543a438a9827fecfd77de60ef'
+
+//0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
